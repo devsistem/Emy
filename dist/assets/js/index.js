432b4f8046fc975e422229407c436718286d3905
Paul Cuichan <ing.paul.cuixan@gmail.com>
2017-12-18 17:49:52 -0500

    Creacion de archivos para produccion

---
+++ dist/assets/js/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+$(document).ready(function () {
+
+  $(function () {
+    $("#slides").slidesjs({
+      /*width: 900,
+        height: 398,*/
+      start: 1,
+      play: {
+        active: true,
+        // [boolean] Generate the play and stop buttons.
+        // You cannot use your own buttons. Sorry.
+        effect: "slide",
+        // [string] Can be either "slide" or "fade".
+        interval: 5000,
+        // [number] Time spent on each slide in milliseconds.
+        auto: true,
+        // [boolean] Start playing the slideshow on load.
+        swap: true,
+        // [boolean] show/hide stop and play buttons
+        pauseOnHover: false,
+        // [boolean] pause a playing slideshow on hover
+        restartDelay: 1500
+        // [number] restart delay on inactive slideshow
+      }
+    });
+  });
+});
+//# sourceMappingURL=maps/index.js.map
